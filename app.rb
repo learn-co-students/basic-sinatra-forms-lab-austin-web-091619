a5c1f9373196b1c1354fa74f3f5df8513bde4267
swdecker <swdecker@gmail.com>
2019-10-07 16:17:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+    post '/team' do
+        # Team name ('name') Coach ('coach') Point Guard ('pg') Shooting Guard ('sg') Power Forward ('pf') Small Forward ('sf') Center ('c')
+        @param_hash = params
+        erb :team
+    end
 
 end
